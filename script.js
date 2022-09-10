d6bfbc8c18fd634102325edf1c692bd517019478
Lucas <Rodriguesdesouzal28@gmail.com>
2022-09-10 08:56:43 -0300

    Envio post e liberar botão confirmar feitas

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-/* const nomeUsuario = prompt("Qual o seu nome ?") */
+const nomeUsuario = prompt("Qual o seu nome ?") 
 let possuiClasse1;
 let possuiClasse2;
 let possuiClasse3;
+
+let modeloEscolhido;
+let gola;
+let tecido;
+
+let dados = {};
+
+
+
 function modeloSelecionado(escolhido){
     const modeloSelecionadoAnterior = document.querySelector(".escolha .selecionado")
+
+    
 
     if(modeloSelecionadoAnterior !== null){
         modeloSelecionadoAnterior.classList.remove("selecionado")
@@ -14,6 +25,10 @@ function modeloSelecionado(escolhido){
 
     possuiClasse1 = escolhido.classList.contains("selecionado"); 
     console.log(possuiClasse1)
+
+
+    modeloEscolhido = document.querySelector(".selecionado .h1t").innerHTML
+    console.log(modeloEscolhido)
     confirmarPedido()
 }
 
@@ -26,6 +41,10 @@ function golaSelecionada(golaEscolhida){
     }
     golaEscolhida.classList.add("selecionado")
     possuiClasse2 = golaEscolhida.classList.contains("selecionado"); 
+
+    gola = document.querySelector(".selecionado .h1").innerHTML
+    console.log(golaEscolhida)
+
     confirmarPedido()
 }
 function tecidoSelecionado(tecidoEscolhido){
@@ -37,21 +56,71 @@ function tecidoSelecionado(tecidoEscolhido){
     }
     tecidoEscolhido.classList.add("selecionado")
     possuiClasse3 = tecidoEscolhido.classList.contains("selecionado"); 
+
+    tecido = document.querySelector(".selecionado .h1.o").innerHTML
+    console.log(tecidoEscolhido)
+
     confirmarPedido()
 }
-/* const link = ""; */
+let inputPreenchido = "";
 function pegarMensagem(){
-    /* link = document.querySelector(".input").value
-    console.log(link) */
-    if(possuiClasse1 & possuiClasse2 & possuiClasse3 === true ){
-    alert("confirmando a encomenda")
-    }    
+    inputPreenchido = document.querySelector("input").value
+    if(possuiClasse1 === true && possuiClasse2 === true && possuiClasse3 === true ){
+    
+    confirmarPedido()
+}
+console.log(inputPreenchido)
 }
 
+console.log(inputPreenchido)
 function confirmarPedido(){
-    if(possuiClasse1 & possuiClasse2 & possuiClasse3 === true ){
+    if(modeloEscolhido === "Camiseta" )modeloEscolhido = "top-tank"
+    if(modeloEscolhido === "T-shirt" )modeloEscolhido = "t-shirt" 
+    if(modeloEscolhido === "Manga longa" )modeloEscolhido = "long"
+
+    if(gola === "Gola V" )gola = "v-neck"
+    if(gola === "Gola Redonda" )gola = "round"
+    if(gola === "Gola polo" )gola = "polo"
+
+    if(tecido === "Seda" )tecido = "silk" 
+    if(tecido === "Algodão" )tecido = "cotton"
+    if(tecido === "Poliéster" )tecido = "polyester"
+
+
+    if(possuiClasse1 === true && possuiClasse2 === true && possuiClasse3 === true && inputPreenchido !== ""){
         const botaoSelecionado = document.querySelector(".confirmar")
         botaoSelecionado.classList.add("botaoSelecionado")
+        
+    dados = {
+            model: `${modeloEscolhido}`,
+            neck: `${gola}`,
+            material: `${tecido}`,
+            image: `${inputPreenchido}`,
+            owner: `${nomeUsuario}`,
+            author: `${nomeUsuario}`
+        }
+
+        const requisicao = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', dados)
+        requisicao.then(sucesso)
+        requisicao.catch(erro)
+
+        
     }
     
+
+    
+ 
+}
+
+
+
+    
+
+ 
+function sucesso(){
+    alert("deu boa ")
+}
+
+function erro(){
+    alert("Ops, aconteceu um erro ! Verefique se a url da imgem esta correta")
 }
